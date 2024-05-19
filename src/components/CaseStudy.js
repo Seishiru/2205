5d6dca2d9dd6d6a3b10660a90082f900cc0a8ac7
Kotegawa <rapharaphael54@gmail.com>
2024-05-19 14:52:58 +0800

    added circles

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import '../css/CaseStudy.css';
+import React, { useState } from 'react';
 
 const CaseStudyPage = () => {
-  return (
-    <div >
-        <h1>Case Study</h1>
-    </div>
-  )
-}
+  const [clickedCircle, setClickedCircle] = useState(null);
 
-export default CaseStudyPage
+  // Function to handle click on circle
+  const handleClick = (index) => {
+    setClickedCircle(index);
+  };
+
+  // Function to create the circles
+  const renderCircles = () => {
+    const circles = [];
+    for (let i = 0; i < 12; i++) {
+      circles.push(
+        <div
+          className={clickedCircle === i ? "circle clicked" : "circle"}
+          key={i}
+          onClick={() => handleClick(i)}
+        ></div>
+      );
+    }
+    return circles;
+  };
+
+  return (
+    <div>
+      <h1>Case Study</h1>
+      <div className="line-container">
+        <div className="line"></div>
+        <div className="circles-container">{renderCircles()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CaseStudyPage;
